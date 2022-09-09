3fc83c08e1143f5489c0379f3098bf52d0b6d0e5
hermkan <hermann.kanga.dev@gmail.com>
2022-09-09 17:23:27 -0400

    rebuild v6

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
 app.use('/v1', api);
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
