6c6af8e75734431aeca602ac2f34f0170162780b
hermkan <hermann.kanga.dev@gmail.com>
2022-09-09 17:39:30 -0400

    rebuild v9

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,12 @@ colors.setTheme({
 app.use(express.json());
 app.use(cors());
 
-app.use(
-  express.static(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'))
-);
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/v1', api);
 app.get('/*', (req, res) => {
-  res.sendFile(path.join('frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-console.log(path.join('frontend', 'dist', 'index.html'));
+
 const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, console.log(`SERVER IS LISTENING ON PORT ${PORT}`.success));
